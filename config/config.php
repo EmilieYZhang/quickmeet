8f99e0c92b65ae9aed1ae93574706762b44230ff
ezhang19 <emilie.zhang@mail.mcgill.ca>
2024-12-15 01:01:24 -0500

    fix

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+define('DB_USER', 'DB_USER');
+define('DB_PASS', 'DB_PASS');
+define('DB_NAME', 'YOUR_DB_NAME');
+
+// email settings (for sending emails)
+define('MAIL_HOST', 'mailhost');
+define('MAIL_USERNAME', 'quickmeet.mcgill@gmail.com');
+define('MAIL_PASSWORD', 'emailpassword');
+define('MAIL_PORT', 000);
+// Set the default timezone for the app
+date_default_timezone_set('America/Toronto');
+?>
