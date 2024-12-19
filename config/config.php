ccd7975257a882d5bf16bae1777991781bc31da6
Japmann Sarin <japmannsarin@Japmanns-MacBook-Pro.local>
2024-12-19 13:28:52 -0500

    email api added

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+// database settings
+//define('DB_HOST', 'mysql.cs.mcgill.ca');
+//define('DB_USER', 'comp307-hkacma');
+//define('DB_PASS', '772MnV0gk6UqmrK');
+//define('DB_NAME', 'fall2024-comp307-hkacma');
+
+// email settings (for sending emails)
+define('MAIL_HOST', 'smtp.gmail.com');
+define('MAIL_USERNAME', 'quickmeet.mcgill@gmail.com');
+define('MAIL_PASSWORD', 'tgssfgzpbgqjvzhw'); // Use App Password
+define('MAIL_PORT', 587);
+
+// Set the default timezone for the app
+//date_default_timezone_set('America/Toronto');
+?>
